7ced74e272f283ea7c35eb269bb1dfd67dc43484
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-03-10 09:56:46 -0600

    fix: hexstring to only string

--- src/nftMinted.ts
+++ src/nftMinted.ts
@@ -6,7 +6,7 @@ export function handleNFTCreated(event: NFTMinted): void {
     let nft = Item.load(event.address.toHexString());
 
     if(!nft) {
-        let nft = new Item(event.address.toHexString() + "-" + event.params.tokenId.toHexString());
+        let nft = new Item(event.address.toHexString() + "-" + event.params.tokenId.toString());
         nft.collection = event.address.toHexString()
         nft.tokenId = event.params.tokenId;
         nft.timestamp = event.block.timestamp;
